d3432c2a16bae2478578787887bdbac4384392df
Akshay khobragade <113614597+Akshaykhobragade3112@users.noreply.github.com>
2024-09-02 22:58:56 +0530

    Update index.js

--- e-commerce-backend/index.js
+++ e-commerce-backend/index.js
@@ -27,7 +27,7 @@ const upload = multer({storage: storage})
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:4000/images/${req.file.filename}`
+        image_url: `https://e-commerce-platform-backend1.onrender.com/images/${req.file.filename}`
     })
 })
 app.use('/images', express.static('upload/images'));
