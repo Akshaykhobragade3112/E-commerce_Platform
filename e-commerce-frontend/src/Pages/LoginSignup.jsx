cfdef22dec6267c8c5ccb814bbc0e5aeb5a68a58
Akshay khobragade <113614597+Akshaykhobragade3112@users.noreply.github.com>
2024-09-02 17:31:23 +0530

    Update LoginSignup.jsx

--- e-commerce-frontend/src/Pages/LoginSignup.jsx
+++ e-commerce-frontend/src/Pages/LoginSignup.jsx
@@ -12,7 +12,7 @@ const LoginSignup = () => {
 
   const login = async () => {
     let dataObj;
-    await fetch('http://localhost:4000/login', {
+    await fetch('https://e-commerce-platform-backend1.onrender.com/login', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
@@ -35,7 +35,7 @@ const LoginSignup = () => {
 
   const signup = async () => {
     let dataObj;
-    await fetch('http://localhost:4000/signup', {
+    await fetch('https://e-commerce-platform-backend1.onrender.com/signup', {
       method: 'POST',
       headers: {
         Accept:'application/form-data',
