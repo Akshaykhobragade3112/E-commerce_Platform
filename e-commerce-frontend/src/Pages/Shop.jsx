6abad0f737c677cbd41ed84646451e4b33c30181
Akshay khobragade <113614597+Akshaykhobragade3112@users.noreply.github.com>
2024-09-02 17:33:59 +0530

    Update Shop.jsx

--- e-commerce-frontend/src/Pages/Shop.jsx
+++ e-commerce-frontend/src/Pages/Shop.jsx
@@ -14,7 +14,7 @@ const Shop = () => {
     fetch('https://e-commerce-platform-backend1.onrender.com/popularinwomen') 
             .then((res) => res.json()) 
             .then((data) => setPopular(data))
-    fetch('http://localhost:4000/newcollections') 
+    fetch('https://e-commerce-platform-backend1.onrender.com/newcollections') 
             .then((res) => res.json()) 
             .then((data) => setNewCollection(data))
     }
