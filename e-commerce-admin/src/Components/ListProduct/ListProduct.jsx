d0cefc08254cab07ea769f267dcbe3031a79e533
Akshay khobragade <113614597+Akshaykhobragade3112@users.noreply.github.com>
2024-09-02 17:41:40 +0530

    Update ListProduct.jsx

--- e-commerce-admin/src/Components/ListProduct/ListProduct.jsx
+++ e-commerce-admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://e-commerce-platform-backend1.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
@@ -16,7 +16,7 @@ const ListProduct = () => {
     }, [])
 
     const removeProduct = async (id) => {
-      await fetch('http://localhost:4000/removeproduct', {
+      await fetch('https://e-commerce-platform-backend1.onrender.com/removeproduct', {
       method: 'POST',
       headers: {
         Accept:'application/json',
@@ -25,7 +25,7 @@ const ListProduct = () => {
       body: JSON.stringify({id:id}),
     })
 
-    fetch('http://localhost:4000/allproducts') 
+    fetch('https://e-commerce-platform-backend1.onrender.com/allproducts') 
     .then((res) => res.json()) 
     .then((data) => setAllProducts(data))
 
