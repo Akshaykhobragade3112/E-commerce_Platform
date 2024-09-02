043f663dc8f9d87bd34e10c40cb00e48c1f2e086
Akshay khobragade <113614597+Akshaykhobragade3112@users.noreply.github.com>
2024-09-02 17:38:49 +0530

    Update AddProduct.jsx

--- e-commerce-admin/src/Components/AddProduct/AddProduct.jsx
+++ e-commerce-admin/src/Components/AddProduct/AddProduct.jsx
@@ -21,7 +21,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append('product', image);
     
-    await fetch('http://localhost:4000/upload', {
+    await fetch('https://e-commerce-platform-backend1.onrender.com/upload', {
       method: 'POST',
       headers: {
         Accept:'application/json',
@@ -34,7 +34,7 @@ const AddProduct = () => {
     if (dataObj.success) {
       product.image = dataObj.image_url;
       console.log(product);
-      await fetch('http://localhost:4000/addproduct', {
+      await fetch('https://e-commerce-platform-backend1.onrender.com/addproduct', {
       method: 'POST',
       headers: {
         Accept:'application/json',
